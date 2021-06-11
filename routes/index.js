f71b8f5508a79f8984ad99d61904804e8e98ae4c
meet <meetgajra55@gmail.com>
2021-06-11 13:46:20 +0530

    star rating fix, reset password commented

--- routes/index.js
+++ routes/index.js
@@ -95,96 +95,96 @@ router.get('/users/:user_id', function(req, res){
 	})
 })
 
-router.get('/forgot', function(req, res){
-	res.render('user/forgotpassword.ejs', { alertmsg : "" });	
-})
+// router.get('/forgot', function(req, res){
+// 	res.render('user/forgotpassword.ejs', { alertmsg : "" });	
+// })
 
 // 1. Create a token
 // 2. find the user and post the token to the user model
 // 3. send the token email to the users email (send details in just an alert box for now)
 
-router.post('/forgot', function(req, res, next){
+// router.post('/forgot', function(req, res, next){
 	
-	async.waterfall([
-		function(done){                                   // -1
-			crypto.randomBytes(20, function(err, buf){
-				var token = buf.toString('hex');
-				done(err, token)
-			})
-		},
+// 	async.waterfall([
+// 		function(done){                                   // -1
+// 			crypto.randomBytes(20, function(err, buf){
+// 				var token = buf.toString('hex');
+// 				done(err, token)
+// 			})
+// 		},
 		
-		function(token, done){                            // -2
-			User.findOne({ email : req.body.email }, function(err, user){
-				console.log(req.body.email);
-				if(!user){
-					req.flash('error','No account with that email address exists!');
-					return res.redirect('/forgot');
-				}
+// 		function(token, done){                            // -2
+// 			User.findOne({ email : req.body.email }, function(err, user){
+// 				console.log(req.body.email);
+// 				if(!user){
+// 					req.flash('error','No account with that email address exists!');
+// 					return res.redirect('/forgot');
+// 				}
 				
-				user.resetPasswordToken   = token;
-				user.resetPasswordExpires = Date.now() + 3600000; // i.e. one hour from now	
+// 				user.resetPasswordToken   = token;
+// 				user.resetPasswordExpires = Date.now() + 3600000; // i.e. one hour from now	
 				
-				user.save(function(err){
-					done(err, user, token);
-				})
-			})
-		},
+// 				user.save(function(err){
+// 					done(err, user, token);
+// 				})
+// 			})
+// 		},
 		
-		function(user, token, done){                      // -3
-			res.render('user/forgotpassword.ejs', { alertmsg : "To Reset Your Password please copy this link in your browser : /reset/"+token })  
-		}
-	], function(err){
-			if(err){
-				return next(err);
-				res.redirect('/forgot');
-			}	
-		}	
-	)
-})
+// 		function(user, token, done){                      // -3
+// 			res.render('user/forgotpassword.ejs', { alertmsg : "To Reset Your Password please copy this link in your browser : /reset/"+token })  
+// 		}
+// 	], function(err){
+// 			if(err){
+// 				return next(err);
+// 				res.redirect('/forgot');
+// 			}	
+// 		}	
+// 	)
+// })
 
-router.get('/reset/:token', function(req, res){
-	//first have to verify that the token is valid(if its actually asigned to a user) and also check if its expired
-	User.findOne({ resetPasswordToken : req.params.token, resetPasswordExpires : {$gt : Date.now()} }, function(err, user){
-		if(!user){
-			req.flash('error', 'The Password Reset token is invalid or has expired!');
-			return res.redirect('/forgot');
-		}
-		res.render('user/resetpassword.ejs', {token : req.params.token});
-	})
-})
+// router.get('/reset/:token', function(req, res){
+// 	//first have to verify that the token is valid(if its actually asigned to a user) and also check if its expired
+// 	User.findOne({ resetPasswordToken : req.params.token, resetPasswordExpires : {$gt : Date.now()} }, function(err, user){
+// 		if(!user){
+// 			req.flash('error', 'The Password Reset token is invalid or has expired!');
+// 			return res.redirect('/forgot');
+// 		}
+// 		res.render('user/resetpassword.ejs', {token : req.params.token});
+// 	})
+// })
 
 
-// 1. find user through token
-// 2. Check password
-// 3. assign new pass to the user using setPassword method(provided by passport, hashes the password by its own) on user
-// 4. clear the resetpasstoken and resetpassexpires field from the user model
-// 5. save the updated user in DB and log in the user
+// // 1. find user through token
+// // 2. Check password
+// // 3. assign new pass to the user using setPassword method(provided by passport, hashes the password by its own) on user
+// // 4. clear the resetpasstoken and resetpassexpires field from the user model
+// // 5. save the updated user in DB and log in the user
 
-router.post('/reset/:token', function(req, res) {
+// router.post('/reset/:token', function(req, res) {
 	
-			User.findOne({ resetPasswordToken : req.params.token }, function(err, user) {       //-1
-				if(!user){
-					req.flash('error', 'The Password Reset token is invalid or has expired!');
-					return res.redirect('/forgot');
-				}
-				if(req.body.password1.length > 0 && req.body.password1 === req.body.password2){                                   //-2    
-					user.setPassword(req.body.password1, function(err){                           //-3
-						user.resetPasswordToken   = undefined;                                     //-4
-						user.resetPasswordExpires = undefined;
+// 			User.findOne({ resetPasswordToken : req.params.token }, function(err, user) {       //-1
+// 				if(!user){
+// 					req.flash('error', 'The Password Reset token is invalid or has expired!');
+// 					return res.redirect('/forgot');
+// 				}
+// 				if(req.body.password1.length > 0 && req.body.password1 === req.body.password2){                                   //-2    
+// 					user.setPassword(req.body.password1, function(err){                           //-3
+// 						user.resetPasswordToken   = undefined;                                     //-4
+// 						user.resetPasswordExpires = undefined;
 						
-						user.save(function(){                                                      //-5
-							req.logIn(user, function(){
-								req.flash('success', 'Password updated Successfully!')
-								res.redirect('/items')	
-							})
-						})
-					})
-				}
-				else {
-					req.flash('error', 'The passwords do not match. Please try again.')
-					return res.redirect('back');
-				}
-			})
-})
+// 						user.save(function(){                                                      //-5
+// 							req.logIn(user, function(){
+// 								req.flash('success', 'Password updated Successfully!')
+// 								res.redirect('/items')	
+// 							})
+// 						})
+// 					})
+// 				}
+// 				else {
+// 					req.flash('error', 'The passwords do not match. Please try again.')
+// 					return res.redirect('back');
+// 				}
+// 			})
+// })
 
 module.exports = router;
